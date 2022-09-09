9acdf4b5cb3638748799bdc196dc278ad4df1c92
Frozenflame <nikgreen496@gmail.com>
2022-09-09 18:07:06 +1200

    cooldown UI working

--- SpaceRouge/Source/SpaceRouge/PlayersUnit.cpp
+++ SpaceRouge/Source/SpaceRouge/PlayersUnit.cpp
@@ -6,6 +6,7 @@
 #include "GameFramework/SpringArmComponent.h"
 #include "Components/SkeletalMeshComponent.h"
 #include "Engine/DataTable.h"
+#include "Engine/GameEngine.h"
 
 
 // Sets default values
@@ -22,20 +23,100 @@ APlayersUnit::APlayersUnit()
 
 	CameraCom = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraCom"));
 	CameraCom->SetupAttachment(SpringArmCom);
+
+	static ConstructorHelpers::FObjectFinder<UDataTable> DT_ActiveAbility(TEXT("DataTable'/Game/DT_ActiveAbility.DT_ActiveAbility'"));
+	if (DT_ActiveAbility.Succeeded())
+	{
+		PlayerActSkills_DT = DT_ActiveAbility.Object;
+	}
+	SKillcooldown.SetNum(6);
 }
 
 // Called when the game starts or when spawned
 void APlayersUnit::BeginPlay()
 {
 	Super::BeginPlay();
+
+	PlayerrActSkills_Structs.SetNum(5);
 	
+	addSkill(0,"Attack");
+	//addSkill(1, "Ice");
+	//addSkill(2, "Mult");
+	//addSkill(3, "Spirt");
+	//addSkill(4, "Lighting");
 }
 
 // Called every frame
 void APlayersUnit::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	
+	if (SkillsOnCooldown.Num() != 0)
+	{
+		
+		for (auto Elem = SkillsOnCooldown.CreateConstIterator(); Elem;)
+		{
+			if (Elem.Value() - DeltaTime <= 0)
+			{
+				UISkillOffCooldown(Elem.Key());
+				SkillsOnCooldown.Remove(Elem.Key());
+				if (GEngine) {
 
+					GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("Remove %f"), Elem.Key()));
+				}
+				//skill off cooldown (good to use)
+			
+		//		//
+			}
+			else
+			{
+				//still on cooldown
+				SkillsOnCooldown[Elem.Key()] = Elem.Value()- DeltaTime;
+				UIUpdateCooldownOnSkill(Elem.Key(), Elem.Value() - DeltaTime);
+				GEngine->AddOnScreenDebugMessage(-1, 0.3f, FColor::Yellow, FString::Printf(TEXT("%f"), Elem.Value() - DeltaTime));
+			}
+			++Elem;
+		}
+
+		/*for (auto Elem : SkillsOnCooldown)
+		{
+			SkillsOnCooldown[Elem.Key] = Elem.Value - DeltaTime;
+			UIUpdateCooldownOnSkill(Elem.Key, Elem.Value - DeltaTime);
+			if (GEngine) {
+
+				//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("%f"), Elem.Value - DeltaTime));
+			}
+		}*/
+	}
+
+}
+void APlayersUnit::addSkill(int _skillLoctation, FName _skillName)
+{
+	if (PlayerActSkills_DT)
+	{
+		
+		static const FString ContextString(TEXT("Act_Ability Context"));
+		PlayerrActSkills_Structs[_skillLoctation] = 
+			PlayerActSkills_DT->FindRow<FAct_Ability>(_skillName, ContextString, true);
+		
+		if (PlayerrActSkills_Structs[_skillLoctation])
+		{
+			UTexture2D* Image = PlayerrActSkills_Structs[_skillLoctation]->Image;
+			UIAddSkill(Image, _skillLoctation);
+			
+			UseSkill(_skillLoctation);
+			if (GEngine) {
+
+				GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Green, FString::Printf(TEXT("%f"), PlayerrActSkills_Structs[_skillLoctation]->Cooldown));
+
+				//float g = PlayerrActSkills_Structs->Damage;
+				//FString Message = FString::Printf(TEXT("%s"),
+				//	(PlayerrActSkills_Structs->Image));
+				//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Green, FString::Printf(TEXT("%f"), SKillcooldown.Num()));
+				//UE_LOG(LogTemp, Warning, TEXT("The Actor's name is %s"), *YourActor->GetName());
+			}
+		}
+	}
 }
 void APlayersUnit::MoveRight(float Value)
 {
@@ -61,6 +142,32 @@ void APlayersUnit::MoveForward(float Value)
 	}
 }
 
+void APlayersUnit::UseSkill(int Value)
+{
+	//if skill is empty : return;
+	//SKillcooldown[Value] = PlayerrActSkills_Structs->Cooldown;
+
+	if (SkillsOnCooldown.Num() > 0)
+	{
+		if (!SkillsOnCooldown.FindKey(Value))
+		{
+			//can't used skill
+			if (GEngine) {
+
+				GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, 
+					FString::Printf(TEXT("skill %f is on cooldown"), Value));
+			}
+			return;
+		}
+	}
+
+	UIUseSkill(Value, PlayerrActSkills_Structs[Value]->Cooldown);
+	SkillsOnCooldown.Add(Value, PlayerrActSkills_Structs[Value]->Cooldown);
+
+	//do skill
+
+}
+
 // Called to bind functionality to input
 void APlayersUnit::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
@@ -68,5 +175,11 @@ void APlayersUnit::SetupPlayerInputComponent(UInputComponent* PlayerInputCompone
 
 	PlayerInputComponent->BindAxis("MoveRight", this, &APlayersUnit::MoveRight);
 	PlayerInputComponent->BindAxis("MoveForward", this, &APlayersUnit::MoveForward);
+	
+	PlayerInputComponent->BindAction<Skill>("skill1", IE_Pressed, this, &APlayersUnit::UseSkill,0);
+	PlayerInputComponent->BindAction<Skill>("skill2", IE_Pressed, this, &APlayersUnit::UseSkill, 1);
+	PlayerInputComponent->BindAction<Skill>("skill3", IE_Pressed, this, &APlayersUnit::UseSkill, 2);
+	PlayerInputComponent->BindAction<Skill>("skill4", IE_Pressed, this, &APlayersUnit::UseSkill, 3);
+	PlayerInputComponent->BindAction<Skill>("skill5", IE_Pressed, this, &APlayersUnit::UseSkill, 4);
 }
 
--- SpaceRouge/Source/SpaceRouge/PlayersUnit.h
+++ SpaceRouge/Source/SpaceRouge/PlayersUnit.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "PossablePlayerStatsStruct.h"
+#include "PossablePlayerStatsStruct.h"
 #include "PlayersUnit.generated.h"
 
 class USkeletalMeshComponent;
@@ -26,17 +27,52 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera")
 		UCameraComponent* CameraCom;
 
-	TArray<FAct_Ability> PossableAttacks;
+	//TArray<FAct_Ability> PossableAttacks;
+	//	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "DataTable")
+	//	FAct_Ability PlayerrActSkills_Structs;
+	TArray <FAct_Ability*> PlayerrActSkills_Structs;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "DataTable")
+	UDataTable* PlayerActSkills_DT;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "skills")
+	TArray<float> SKillcooldown;
+	TMap<int, float> SkillsOnCooldown;
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
+	DECLARE_DELEGATE_OneParam(Skill, int32);
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+	//
+	void addSkill(int _skillLoctation,FName _skillName);
+
+	//inputs functions
 	void MoveRight(float Value);
 	void MoveForward(float Value);
+	void UseSkill(int Value);
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	//
+	UFUNCTION(BlueprintImplementableEvent)
+		void UISetup(UTexture2D* SkillChangeImage);
+	//
+	UFUNCTION(BlueprintImplementableEvent)
+		void UIChange(int SkillChange, UTexture2D* SkillChangeImage);
+	
+	
+	//skill changed or got added
+	UFUNCTION(BlueprintImplementableEvent)
+		void UIAddSkill( UTexture2D* SkillChangeImage, int _skill);
+	//skill was used and its cooldown
+	UFUNCTION(BlueprintImplementableEvent)
+		void UIUseSkill(int _skill, float _Skillcooldown);
+	//skill new cooldown
+	UFUNCTION(BlueprintImplementableEvent)
+		void UIUpdateCooldownOnSkill(int _skill, float _Skillcooldown);
+	//skill off cooldown
+	UFUNCTION(BlueprintImplementableEvent)
+		void UISkillOffCooldown(int _skill);
+	
 };
